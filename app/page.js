03e18283385db61f64b6e3bb1587201e3ded8180
TanishqKashla <tanishqkashla11@gmail.com>
2026-03-09 11:25:44 +0530

    blogs section added

--- app/page.js
+++ app/page.js
@@ -3,13 +3,15 @@ import CTASection from './components/CTASection'
 import Hero from './components/Hero'
 import PhilosophySection from './components/PhilosophySection'
 import StrategicOfferings from './components/StrategicOfferings'
+import BlogsSection from './components/BlogsSection'
 
 const page = () => {
   return (
     <div>
       <Hero/>
       <PhilosophySection/>
-      <StrategicOfferings/>
+      <StrategicOfferings />
+      <BlogsSection/>
       <CTASection/>
     </div>
   )
